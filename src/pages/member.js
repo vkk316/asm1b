dd8c849cf837927bfde0d27ea5dea5092d88d7a7
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2022-08-10 05:06:48 +0700

    page query

--- src/pages/member.js
+++ src/pages/member.js
@@ -2,14 +2,29 @@
 
 import * as React from "react"
 import Layout from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
 
-export default function MemberInfoPage (props) {
+export default function MemberInfoPage({data}) {
 
-  const data = props.pageContext.member // เข้าถึงข้อมูลจากตัวแปร data
-
-  return (
-    <Layout>
-      {/* เขียนโค้ดโลดดดด */}
-    </Layout>
-  )
+  return <Layout>{console.log(data.directus)}</Layout>
 }
+
+export const query = graphql`
+query MyQuery($student_id: String){
+  directus {
+    Member(filter: { student_id: { _eq: $student_id } }) {
+      student_id
+      first_name
+      last_name
+      enrollment {
+        Section_id {
+          number
+          subject {
+            title
+          }
+        }
+      }
+    }
+  }
+}
+`;
--- src/pages/subject-detail.js
+++ src/pages/subject-detail.js
@@ -1,13 +1,55 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
 
-export default function SubjectDetailPage (props) {
-
-  const data = props.pageContext.subject // เข้าถึงข้อมูลจากตัวแปร data
-
-  return (
-    <Layout>
-      {/* เขียนโค้ดโลดดดด */}
-    </Layout>
-  )
+export default function SubjectDetailPage(props) {
+  const subject_id = props.pageContext.subject_id
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     Subject {
+  //       id
+  //       title
+  //       sections {
+  //         periods {
+  //           day
+  //           start
+  //           end
+  //         }
+  //         number
+  //         student {
+  //           Member_id {
+  //             student_id
+  //             first_name
+  //             last_name
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  return <Layout>{console.log(subject_id)}</Layout>
 }
+
+// export const query = (context) => graphql`
+//   query {
+//     Subject {
+//       id
+//       title
+//       sections {
+//         periods {
+//           day
+//           start
+//           end
+//         }
+//         number
+//         student {
+//           Member_id {
+//             student_id
+//             first_name
+//             last_name
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
