e84291f90956578efde2e39cda519e6dfb770296
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2022-08-09 11:38:03 +0700

    fix import static image

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,5 +1,6 @@
 import * as React from "react"
-import { Link, useStaticQuery, graphql, StaticImage} from "gatsby-plugin-image"
+import { Link, useStaticQuery, graphql} from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Profile = (props) => {
 
