0b90779ae19e3219875345d6f800ab2cf2374555
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2022-08-10 15:18:09 +0700

    success p3,4

--- src/pages/subject-detail.js
+++ src/pages/subject-detail.js
@@ -2,23 +2,67 @@
 
 import * as React from "react"
 import Layout from "../components/layout"
-import {graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 export default function SubjectDetailPage({ data }) {
-
   const subject = data.directus.Subject[0] //เข้าถึงข้อมูลของสมาชิกผ่านตัวแปร subject
 
-  return <Layout>
-    {
-      // Write your code here
-    }
-  </Layout>
+  return (
+    <Layout>
+      {
+        <section className="py-5 container">
+          <div>
+            <h2>{subject.id}</h2> <h2>{subject.title}</h2>
+          </div>
+          <hr />
+          {subject.sections.map(sec => (
+            <SectionCard data={sec} key={sec.number}></SectionCard>
+          ))}
+        </section>
+      }
+    </Layout>
+  )
+}
+
+export function SectionCard(props) {
+  const sec = props.data
+  return (
+    <div class="card w-100 mt-4">
+      <div class="card-body">
+        <h5 class="card-title">Section {sec.number}</h5>
+        {sec.periods.map(p => (
+          <h6>
+            {p.day} {p.start} - {p.end}
+          </h6>
+        ))}
+        <hr />
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Student ID</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sec.student.map(std => (
+              <tr>
+                <th scope="row">{std.Member_id.student_id}</th>
+                <td>{std.Member_id.first_name}</td>
+                <td>{std.Member_id.last_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
 export const query = graphql`
-  query CatQuery($subject_id: String){
+  query CatQuery($subject_id: String) {
     directus {
-      Subject(filter: { id: { _eq: $subject_id } }){
+      Subject(filter: { id: { _eq: $subject_id } }) {
         id
         title
         sections {
--- src/pages/subject.js
+++ src/pages/subject.js
@@ -2,13 +2,14 @@
 
 import * as React from "react"
 import Layout from "../components/layout"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 export default function MemberInfoPage() {
   const data = useStaticQuery(graphql`
     query {
       directus {
         Subject {
+          id
           title
           sections {
             number
@@ -23,8 +24,35 @@ export default function MemberInfoPage() {
   return (
     <Layout>
       {
-        // Write your code here.
+        <section className="py-5 container">
+          {subjects.map(subject => (
+            <MinSubjectCard subject={subject} key={subject.id}></MinSubjectCard>
+          ))}
+          <div></div>
+        </section>
       }
     </Layout>
+  )
+}
+
+export function MinSubjectCard(props) {
+  return (
+    <div class="card my-2">
+      <div class="card-body">
+        <div class="row no-gutters">
+          <div class="col-12 col-sm-6 col-md-8">
+            <b>{props.subject.id}</b> {props.subject.title}{" "}
+          </div>
+          <div class="col-6 col-md-4 text-right">
+            <Link
+              to={`${props.subject.id}`}
+              className="btn btn-primary"
+            >
+              Detail
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
